f5d789177b337ba10fa3534b879f5c2d4ce27bce
0xc0debabe <kongminoo@naver.com>
2024-11-01 02:09:58 +0900

    [24/10/29] @Aspect AOP

---
+++ src/test/java/hello/proxy/advisor/MultiAdvisorTest.java
@@ -0,0 +1,64 @@
+package hello.proxy.advisor;
+
+import hello.proxy.common.advice.TimeAdvice;
+import hello.proxy.common.service.ServiceImpl;
+import hello.proxy.common.service.ServiceInterface;
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+
+@Slf4j
+public class MultiAdvisorTest {
+
+    @Test
+    void multiAdvisorTest1() throws Exception {
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice1());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy1 = (ServiceInterface) proxyFactory.getProxy();
+
+        ProxyFactory proxyFactory1 = new ProxyFactory(proxy1);
+        DefaultPointcutAdvisor advisor1 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice2());
+        proxyFactory1.addAdvisor(advisor1);
+        ServiceInterface proxy2 = (ServiceInterface) proxyFactory1.getProxy();
+        proxy2.save();
+    }
+
+    @Test
+    void multiAdvisorTest2() throws Exception {
+        DefaultPointcutAdvisor advisor1 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice1());
+        DefaultPointcutAdvisor advisor2 = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice2());
+
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory1 = new ProxyFactory(target);
+
+        proxyFactory1.addAdvisor(advisor2);
+        proxyFactory1.addAdvisor(advisor1);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory1.getProxy();
+
+        proxy.save();
+    }
+
+    static class Advice1 implements MethodInterceptor {
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            log.info("advice1 호출");
+            return invocation.proceed();
+        }
+    }
+
+
+    static class Advice2 implements MethodInterceptor {
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            log.info("advice2 호출");
+            return invocation.proceed();
+        }
+    }
+}
